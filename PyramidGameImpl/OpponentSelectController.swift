63e4bc04d7c319a3eae50baa91f9e4fac51f19dc
Michael LeKander <omgtallmonster@gmail.com>
2016-03-24 12:16:04 +0100

    Remove easter eggs. :(

--- PyramidGameImpl/OpponentSelectController.swift
+++ PyramidGameImpl/OpponentSelectController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class OpponentSelectController: UIViewController {
-    @IBAction func test2(sender: UIButton) {
-        self.performSegueWithIdentifier("easterWin", sender: self)
-    }
-    @IBAction func test1(sender: UIButton) {
-        self.performSegueWithIdentifier("easterLose", sender: self)
-    }
+//    @IBAction func test2(sender: UIButton) {
+//        self.performSegueWithIdentifier("easterWin", sender: self)
+//    }
+//    @IBAction func test1(sender: UIButton) {
+//        self.performSegueWithIdentifier("easterLose", sender: self)
+//    }
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
         if let pvc = segue.destinationViewController as? PyramidViewController {
@@ -28,19 +28,20 @@ class OpponentSelectController: UIViewController {
                     print("PANIC!!! Unknown segue identifier:\(id)")
                 }
             }
-        } else if let rvc = segue.destinationViewController as? ResultViewController {
-            if let id = segue.identifier {
-                if id == "easterWin" {
-                    rvc.lScore = 20
-                    rvc.rScore = 10
-                } else if id == "easterLose" {
-                    rvc.lScore = 10
-                    rvc.rScore = 20
-                } else {
-                    print("PANIC!!! Unknown segue identifier:\(id)")
-                }
-            }
         }
+//        else if let rvc = segue.destinationViewController as? ResultViewController {
+//            if let id = segue.identifier {
+//                if id == "easterWin" {
+//                    rvc.lScore = 20
+//                    rvc.rScore = 10
+//                } else if id == "easterLose" {
+//                    rvc.lScore = 10
+//                    rvc.rScore = 20
+//                } else {
+//                    print("PANIC!!! Unknown segue identifier:\(id)")
+//                }
+//            }
+//        }
         super.prepareForSegue(segue, sender: sender)
         
     }
